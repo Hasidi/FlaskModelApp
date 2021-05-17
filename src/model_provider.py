b2a0d0270e8ec45ea0723c4f1048622e6990246e
nhasidi <nhasidi@gmail.com>
2021-05-18 02:43:47 +0300

    Revert "add reader-writer-lock to model_provider"
    This reverts commit 6a0d7eb6

--- src/model_provider.py
+++ src/model_provider.py
@@ -3,37 +3,29 @@ import subprocess
 import uuid
 from functools import lru_cache
 
-from readerwriterlock import rwlock
-
 MODEL_OUTPUT_FOLDER = 'model_output'
 MODEL_FOLDER = 'model'
 MODEL_FILE_PATH = f'{MODEL_FOLDER}/test_model.py'
-LOCK_TIMEOUT_SEC = 3
 
 
 class ModelProvider(object):
     def __init__(self):
         if not os.path.exists(MODEL_OUTPUT_FOLDER):
             os.makedirs(MODEL_OUTPUT_FOLDER)
-        self._lock = rwlock.RWLockFairD()
 
     def save_model(self, model: str) -> bool:
         """
         :param string model: code as string
         :return bool: if succeeded
         """
-        w_lock = self._lock.gen_wlock()
-        if w_lock.acquire(timeout=LOCK_TIMEOUT_SEC):
-            try:
-                with open(MODEL_FILE_PATH, 'w') as f:
-                    f.write(model)
-                return True
-            except:
-                return False
-            finally:
-                self.get_prediction.cache_clear()
-                w_lock.release()
-        return False
+        try:
+            with open(MODEL_FILE_PATH, 'w') as f:
+                f.write(model)
+            return True
+        except:
+            return False
+        finally:
+            self.get_prediction.cache_clear()
 
     @lru_cache(maxsize=1000)
     def get_prediction(self, data: str) -> str:
@@ -42,18 +34,13 @@ class ModelProvider(object):
         :return: script output
         :rtype: str
         """
-        r_lock = self._lock.gen_rlock()
-        if r_lock.acquire(timeout=LOCK_TIMEOUT_SEC):
-            output_path = f'{MODEL_OUTPUT_FOLDER}/{str(uuid.uuid4())}.txt'
-            try:
-                exit_code = subprocess.call(['python', MODEL_FILE_PATH, output_path, data], timeout=15)
-            except:
-                exit_code = 1
-            finally:
-                r_lock.release()
-            if exit_code != 0:
-                return ''
-            with open(output_path, 'r') as f:
-                return f.read()
-        return ''
-
+        output_path = f'{MODEL_OUTPUT_FOLDER}/{str(uuid.uuid4())}.txt'
+        try:
+            exit_code = subprocess.call(['python', MODEL_FILE_PATH, output_path, data], timeout=3)
+        except:
+            exit_code = 1
+        if exit_code != 0:
+            return ''
+        with open(output_path, 'r') as f:
+            r = f.read()
+        return r
