2a6be9cfa3e2e47b558b5df8d12b9dc2b8f382a2
nhasidi <nhasidi@gmail.com>
2021-05-17 23:27:54 +0300

    update prep_ubuntu.sh

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -1,3 +1,5 @@
+export APP_PORT=80
+echo -e "export APP_PORT=80" >> ~/.bashrc
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.7
@@ -7,6 +9,5 @@ sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
 sudo apt install docker.io
 sudo usermod -aG docker $USER
 newgrp docker
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
-export APP_PORT=80
-echo -e "export APP_PORT=80" >> ~/.bashrc
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
